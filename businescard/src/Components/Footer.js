0f1e7f6e4192e1005b75d67e7ca2f1f1749f4e2e
nadezdatsygankova <nadiapritchina@gamil.com>
2022-03-23 19:27:57 -0400

    add logo

--- businescard/src/Components/Footer.js
+++ businescard/src/Components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import TwitterLogo from '../images/twitter_icon.svg';
+import FacebookLogo from '../images/facebook_icon.svg';
+import InstagramLogo from '../images/instagram_icon.svg';
+import GitHubLogo from '../images/gitHub_icon.svg';
 
 export default function Footer() {
     return (
         <div className='footer'>
-            <img src='../images/twitter_icon.svg' alt='icon' />
-            <img src='../images/facebook_icon.svg' alt='icon' />
-            <img src='../images/instagram_icon.svg' alt='icon' />
-            <img src='../images/gitHub_icon.svg' alt='icon' />
+            <img src={TwitterLogo} alt="React Logo" />
+            <img src={FacebookLogo} alt="React Logo" />
+            <img src={InstagramLogo} alt="React Logo" />
+            <img src={GitHubLogo} alt="React Logo" />
         </div>
     )
 }
--- businescard/src/Components/Info.js
+++ businescard/src/Components/Info.js
@@ -1,20 +1,22 @@
 import React from 'react'
+import GmailLogo from '../images/icon.svg';
+import LinkedInLogo from '../images/linkedIn_icon.svg';
 
 export default function Info() {
     return (
         <div>
             <div className='imageInfo'>
             </div>
-            <div>
+            <div className='blockInfo'>
                 <h1> Nadia Tsygankova </h1>
                 <h3 className="nameInfo"> Frontend Developer</h3>
                 <h5 className='WebInfo'>tsygankovnadia@gamil.com</h5>
                 <div className='conteinerButtons'>
                     <button className="buttonEmail">
-                        <img src='../images/icon.svg' />
+                        <img src={GmailLogo} alt="Gmail Logo" />
                         Email</button>
                     <button className="buttonLinkedIn">
-                        <img src='../images/linkedIn_icon.svg' alt='icon' />
+                        <img src={LinkedInLogo} alt="LinkedIn Logo" />
                         LinkedIn</button>
                 </div>
             </div>
