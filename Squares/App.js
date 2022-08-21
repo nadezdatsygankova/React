b46c4d2e61b93059cc7fe4005b41630569f0b42e
nadezdatsygankova <nadiapritchina@gamil.com>
2022-08-21 16:50:07 -0400

    add squares function

--- Squares/App.js
+++ Squares/App.js
@@ -5,8 +5,19 @@ import Box from './Box'
 export default function App(props) {
     const [squares, setSquares] = React.useState(boxes)
 
+    function toggle(id){
+      setSquares((prevSquare) =>{
+        return prevSquare.map((item) =>{
+          return item.id === id ? {...item, on: !item.on} : item
+        })
+      })
+    }
     const squareElements = squares.map(square => (
-        <Box key={square.id} on = {square.on}/>
+        <Box
+        key = {square.id}
+        id = {square.id}
+        on = {square.on}
+        toggle = {toggle}/>
     ))
     return (
         <main>
--- Squares/Box.js
+++ Squares/Box.js
@@ -2,16 +2,14 @@ import React from "react"
 
 
 export default function Box(props) {
-const [value, setValue] = props.on
+
 
   const styles = {
     backgroundColor: value ? "#222222" : "#cccccc"
   }
 
-  function toggle(){
-    setValue( (prevValue) => !prevValue)
-  }
+
   return (
-    <div className="box" style={styles} onClick = {toggle}> </div>
+    <div className="box" style={styles} onClick = {()=>props.toggle(props.id)}> </div>
   )
 }
