1207bc626b90afbfdf606a074fe273984067e9bb
nadezdatsygankova <nadiapritchina@gamil.com>
2022-08-21 21:18:29 -0400

    feat: form

--- Jokes/Joke.js
+++ Jokes/Joke.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-export default function Joke (props){
-    return(
+export default function Joke(props) {
+
+    const [isShown, setIsShown] = React.useState('false')
+    function toggle() {
+        setIsShown((prevValue) => !prevValue)
+    }
+
+    return (
         <div className='block'>
-         {props.setup && <h3>Setup: {props.setup}</h3>}
-           <p> {props.punchline}</p>    
+            {props.setup && <h3>Setup: {props.setup}</h3>}
+            {isShown && <p> {props.punchline}</p>}
+            <button onClick={toggle}> {isShown ? 'Show' : 'Hide'} Punchline </button>
         </div>
     )
 }
